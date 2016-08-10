bffd441534a45cc9027b56e6348fd2b411ab7256
Fernando <hidabe@gmail.com>
2016-08-10 11:52:15 +0000

    Añadido easyadmin

---
+++ src/AppBundle/Controller/PrivateController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PrivateController extends Controller
+{
+    /**
+     * @Route("/nuevoTrayecto", name="private_nuevoTrayecto")
+     */
+    public function nuevoTrayectoAction(Request $request)
+    {
+        /**
+         * Muestra un formulario para rellenar un nuevo Trayecto
+         * 
+         * 
+         * 1. Habría que copiar los twig de nuevoTrayecto.html.twig
+         * 2. Y indicar en el render que se muestren
+         * 
+         * */
+        die("Pendiente de hacer: Nuevo Trayecto");
+    }
+    
+    /**
+     * @Route("/publicarTrayecto", name="private_publicarTrayecto")
+     */ 
+    public function publicarTrayectoAction(Request $request)
+    {
+        /**
+         * Guarda los datos enviados por el formulario nuevoTrayecto
+         * 
+         * 1. Habría que guardar los datos recibiendos en $_GET en un nuevoTrayecto
+         * 2. Podríamos poner una redirección a HomeAction (pantalla principal) o a ListAction (a la pantalla de listado de trayectos)
+         * 
+         **/ 
+        die("Pendiente de hacer");
+        //return $this->render('building/index.html.twig');
+    }
+}
--- src/AppBundle/Controller/PublicController.php
+++ src/AppBundle/Controller/PublicController.php
@@ -16,4 +16,19 @@ class PublicController extends Controller
         return $this->render('home/index.html.twig', array(
         ));
     }
+    
+    /**
+     * @Route("/list", name="public_list") 
+     */
+    public function listAction(Request $request)
+    {
+        /**
+         * Meteríamos el código de controller/list.php en esta acción de este controlador
+         * Pasaríamos el fichero list.html y los includes que tiene a este proyecto
+         * Haríamos un render a list.html pasando como parámetro el listado de trayectos a mostrar
+         * 
+        **/
+        //die("Listado: Pendiente de hacer");
+        return $this->render('building/index.html.twig');
+    }
 }
