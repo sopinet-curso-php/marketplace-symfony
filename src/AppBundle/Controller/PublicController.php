0ff7d1a27fd6815c967d80b52d88a0947b1f2281
hidabe <fernando@sopinet.com>
2016-08-12 08:16:39 +0200

    7.6. - Completamos acción list

--- src/AppBundle/Controller/PublicController.php
+++ src/AppBundle/Controller/PublicController.php
@@ -22,13 +22,40 @@ class PublicController extends Controller
      */
     public function listAction(Request $request)
     {
-        /**
-         * Meteríamos el código de controller/list.php en esta acción de este controlador
-         * Pasaríamos el fichero list.html y los includes que tiene a este proyecto
-         * Haríamos un render a list.html pasando como parámetro el listado de trayectos a mostrar
-         * 
-        **/
-        //die("Listado: Pendiente de hacer");
-        return $this->render('building/index.html.twig');
+        $entityManager = $this->getDoctrine()->getManager();
+        $repositorioTrayecto = $entityManager->getRepository("AppBundle:Trayecto");
+        $trayectos = $repositorioTrayecto->findAll();
+
+        // Si no se indica un filtro para la fecha, se muestran todos los trayectos
+        if ($request->get("posted") != "" || $request->get('country') != "") {
+            $parameters=array();
+            $queryBuilder = $repositorioTrayecto->createQueryBuilder('trayecto');
+            if ($request->get("country") != "") {
+                $queryBuilder->where('trayecto.origen = :country')
+                    ->orWhere('trayecto.destino = :country');
+                $parameters['country'] = $request->get("country");
+            }
+            if($request->get("posted") != ""){
+                if ($request->get("country") != "") {
+                    $queryBuilder->andWhere('trayecto.createdAt > :posted');
+                }
+                else {
+                    $queryBuilder->where('trayecto.createdAt > :posted');
+                }
+                $date=new \DateTime();
+                $date->modify('-'.$request->get('posted').' day');
+                $parameters['posted']=$date;
+            }
+            $trayectosFiltrados =$queryBuilder->setParameters($parameters)
+                ->getQuery()->execute();
+        } else {
+            $trayectosFiltrados = $trayectos;
+        }
+
+        return $this->render('list/index.html.twig', array(
+            'trayectos' => $trayectosFiltrados,
+            'posted' => $request->get('posted'),
+            'country' => $request->get('country')
+        ));
     }
 }
